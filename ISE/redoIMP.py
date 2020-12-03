cfa693b807e0dea5ca87a63be8a0a26431d3a96e
“U81-A” <Xi_Anne@outlook.com>
2020-12-04 00:23:26 +0800

    nothing, works out nothing

--- ISE/redoIMP.py
+++ ISE/redoIMP.py
@@ -7,6 +7,9 @@ import random
 import numpy as np
 import math
 
+node_num = 0
+edge_num = 0
+
 
 class Node(object):
     def __init__(self, index: int):
@@ -26,6 +29,67 @@ class Node(object):
 
     def set_visited(self):
         self.visited = True
+
+def node_selection(mathcal_R, k):
+    S_star_k = set()
+    for i in range(1,)
+
+def logcnk(n, k):
+    up = 1
+    down = 1
+    for i in range(n - k + 1, n + 1):
+        up = up * i
+    for j in range(1, k + 1):
+        down = down * j
+    res = up / down
+    return math.log(res)
+
+
+def get_lambda_prime(epsilon_prime, k, mathcal_l):
+    lambda_prime = ((2 + 2 * epsilon_prime / 3) * (
+            logcnk(node_num, k) + mathcal_l * math.log(node_num) + math.log(math.log2(node_num))) * node_num) / pow(
+        epsilon_prime, 2)
+    return lambda_prime
+
+
+def get_rr_set(node_index: int):
+    rr_set = set()
+    rr_set.add(node_index)
+    return rr_set
+
+
+def sampling(graph: list, k: int, epsilon: float, mathcal_l: float):
+    mathcal_R = set()
+    LB = 1
+    epsilon_prime = math.sqrt(2) * epsilon
+    for i in range(1, int(math.log2(node_num - 1)) + 1):
+        x = node_num / (math.pow(2, i))
+        lambda_prime = get_lambda_prime(epsilon_prime, k, mathcal_l)
+        theta_i = lambda_prime / x
+        while len(mathcal_R) <= theta_i:
+            select_node_index = random.randint(1, node_num)
+            rr_set = get_rr_set(select_node_index)
+            mathcal_R.add(rr_set)
+        mathcal_S_i, F_mathcal_R = node_selection(mathcal_R)
+        if node_num * F_mathcal_R >= (1 + epsilon_prime) * x:
+            LB = node_num * F_mathcal_R / (1 + epsilon_prime)
+            break
+    alpha = math.sqrt(mathcal_l * math.log(node_num) + math.log(2))
+    beta = math.sqrt((1 - 1 / math.e) * (logcnk(node_num, k) + mathcal_l * math.log(node_num) + math.log(2)))
+    lambda_star = 2 * node_num * pow(((1 - 1 / math.e) * alpha + beta), 2) * pow(epsilon, -2)
+    theta = lambda_star / LB
+    while len(mathcal_R) <= theta:
+        select_node_index = random.randint(1, node_num)
+        rr_set = get_rr_set(select_node_index)
+        mathcal_R.add(rr_set)
+    return mathcal_R
+
+
+def IMM(graph: list, k: int, epsilon: float, mathcal_l: float):
+    mathcal_l = mathcal_l * (1 + math.log(2) / math.log(node_num))
+    mathcal_R = sampling(graph, k, epsilon, mathcal_l)
+    mathcal_S_k = node_selection(mathcal_R, k)
+    return mathcal_S_k
 
 
 if __name__ == '__main__':
@@ -50,7 +114,9 @@ if __name__ == '__main__':
     network_info = file_network[0].split(' ')
 
     graph = []
+    global node_num
     node_num = int(network_info[0]) + 1
+    global edge_num
     edge_num = int(network_info[1].replace("\n", ""))
 
     for node_counter in range(int(network_info[0]) + 1):
@@ -66,11 +132,11 @@ if __name__ == '__main__':
         node_child.in_degree += 1
 
     epsilon = 0.5
-    l = 1
+    mathcal_l = 1
 
-    seeds = IMM(epsilon, l)
+    seeds = IMM(epsilon, mathcal_l)
     for seed in seeds:
-        print(seed + 1)
+        print(seed)
 
     '''
     程序结束后强制退出，跳过垃圾回收时间, 如果没有这个操作会额外需要几秒程序才能完全退出
